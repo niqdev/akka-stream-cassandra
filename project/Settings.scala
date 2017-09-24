055e2137f1cbd08ca4488a328c920c5a693d086a
niqdev <niqdev@gmail.com>
2017-09-24 19:47:00 +0100

    config sbt-release-early plugin

--- project/Settings.scala
+++ project/Settings.scala
@@ -1,17 +1,27 @@
 import Dependencies._
+import ch.epfl.scala.sbt.release.ReleaseEarlyPlugin.autoImport._
+import com.typesafe.sbt.SbtPgp.autoImport._
 import sbt.Keys._
+import sbt._
 
 object Settings {
 
   lazy val commonSettings = Seq(
     organization := "com.github.niqdev",
-    scalaVersion := V.scala,
-    version := "0.1.0"
+    scalaVersion := V.scala
   )
 
   lazy val libSettings = commonSettings ++ Seq(
     name := "akka-stream-cassandra",
-    libraryDependencies ++= libDependencies
+    libraryDependencies ++= libDependencies,
+    licenses := Seq("MIT" -> url("https://opensource.org/licenses/MIT")),
+    homepage := Some(url(s"https://github.com/niqdev/akka-stream-cassandra")),
+    scmInfo := Some(
+      ScmInfo(url(s"https://github.com/niqdev/akka-stream-cassandra"),
+        "scm:git:git@github.com:niqdev/akka-stream-cassandra.git")),
+    pgpPublicRing := file("./travis/local.pubring.asc"),
+    pgpSecretRing := file("./travis/local.secring.asc"),
+    releaseEarlyWith := BintrayPublisher
   )
 
   lazy val exampleSettings = commonSettings ++ Seq(
